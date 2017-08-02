9f91db47a6bdb880378ad5b54d13925e90f57b84
John Holdsworth <github@johnholdsworth.com>
2017-08-02 02:26:46 +0100

    Revert "Test"
    This reverts commit ab6e8dc59a026ea696e693d9c751c451e7ffd49e.

---
+++ app/src/main/java/org/genie/com_jh/SwiftHello_ListenerProxy.java
@@ -0,0 +1,39 @@
+
+/// interface com.jh.SwiftHello$Listener ///
+
+package org.genie.com_jh;
+
+@SuppressWarnings("JniMissingFunction")
+public class SwiftHello_ListenerProxy implements com.jh.SwiftHello.Listener {
+
+    long swiftObject;
+
+    SwiftHello_ListenerProxy( long swiftObject ) {
+        this.swiftObject = swiftObject;
+    }
+
+    /// public abstract void com.jh.SwiftHello$Listener.setCacheDir(java.lang.String)
+
+    public native void __setCacheDir( java.lang.String cacheDir );
+
+    public void setCacheDir( java.lang.String cacheDir ) {
+        __setCacheDir( cacheDir );
+    }
+
+    /// public abstract void com.jh.SwiftHello$Listener.processNumber(double)
+
+    public native void __processNumber( double number );
+
+    public void processNumber( double number ) {
+        __processNumber( number );
+    }
+
+    /// public abstract void com.jh.SwiftHello$Listener.processText(java.lang.String)
+
+    public native void __processText( java.lang.String text );
+
+    public void processText( java.lang.String text ) {
+        __processText( text );
+    }
+
+}
